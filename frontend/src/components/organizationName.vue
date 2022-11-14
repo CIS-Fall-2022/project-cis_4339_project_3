beeeb60327a7ccc769770c20c8782e999ec3effb
calvinguyen <cnguyenr42@gmail.com>
2022-11-13 19:50:48 -0600

    org name now shows up dynamically

---
+++ frontend/src/components/organizationName.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1 class="mr-20 text-3xl text-white">
+        {{ orgName }}
+    </h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            orgName: ""
+        };
+    },
+    mounted() {
+        let apiURL = import.meta.env.VITE_ROOT_API + `/organizationdata/id/` + import.meta.env.VITE_ORG_ID;
+        axios.get(apiURL).then((resp) => {
+            let data = resp.data;
+            this.orgName = data[0].organizationName;
+        });
+    },
+};
+</script>
